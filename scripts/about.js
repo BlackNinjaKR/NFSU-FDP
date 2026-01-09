9f2f4e87cd5eed67a888ceb022ab5749c53461a4
Anik Dasgupta <Dasguptaanik123@gmail.com>
2026-01-10 02:05:56 +0530

    Website Template

---
+++ scripts/about.js
@@ -0,0 +1,25 @@
+  /*document.addEventListener('DOMContentLoaded', () => {
+    const sr = ScrollReveal();
+
+    sr.reveal('.about-text', {
+      origin: 'left',
+      distance: '50px',
+      duration: 1200,
+      delay: 100,
+      easing: 'ease-in-out',
+      reset: false
+    });
+
+    sr.reveal('.about-image', {
+      origin: 'right',
+      distance: '50px',
+      duration: 1200,
+      delay: 300,
+      easing: 'ease-in-out',
+      reset: false
+    });
+  });
+*/
+
+
+  //Bugged
