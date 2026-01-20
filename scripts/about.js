ed5ef6382f97c8003b005f1f4a975ab291e86764
Anik Dasgupta <Dasguptaanik123@gmail.com>
2026-01-20 15:19:56 +0530

    Accordion

--- scripts/about.js
+++ scripts/about.js
@@ -23,3 +23,10 @@
 
 
   //Bugged
+
+  document.querySelectorAll('.accordion-header').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const item = btn.parentElement;
+    item.classList.toggle('active');
+  });
+});
