d0f095e38d2d3b2c5ba91ad9b3935b33dc65623d
Anik Dasgupta <Dasguptaanik123@gmail.com>
2026-01-21 21:17:26 +0530

    Button UI

--- scripts/about.js
+++ scripts/about.js
@@ -30,3 +30,11 @@
     item.classList.toggle('active');
   });
 });
+
+function openTab(tabId) {
+  document.querySelectorAll('.tab-content').forEach(t => t.classList.remove('active'));
+  document.querySelectorAll('.tab-btn').forEach(b => b.classList.remove('active'));
+
+  document.getElementById(tabId).classList.add('active');
+  event.currentTarget.classList.add('active');
+}
